e1652b983019a8339c2644cd12e673cd195127a8
Mihnea <mihnealopataru@gmail.com>
2020-05-31 13:21:29 +0300

    Test 02

---
+++ silk/silk/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using silk.Data;
+
+[assembly: HostingStartup(typeof(silk.Areas.Identity.IdentityHostingStartup))]
+namespace silk.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
