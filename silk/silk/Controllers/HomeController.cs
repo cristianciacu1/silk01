bc08a9312ea9a0c1f22d7c44c1c362f57f69de83
Mihnea <mihnealopataru@gmail.com>
2020-06-11 19:42:39 +0300

    HomePage (refacut)

--- silk/silk/Controllers/HomeController.cs
+++ silk/silk/Controllers/HomeController.cs
@@ -23,7 +23,7 @@ namespace silk.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult Browse()
         {
             return View();
         }
