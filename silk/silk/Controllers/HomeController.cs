2e96ef7b3bbad3be46838259339cc66a6462afb6
Mihnea <mihnealopataru@gmail.com>
2020-06-15 12:06:53 +0300

    Linked pages + Responsive buttons

--- silk/silk/Controllers/HomeController.cs
+++ silk/silk/Controllers/HomeController.cs
@@ -23,13 +23,6 @@ namespace silk.Controllers
             return View();
         }
 
-
-        [HttpGet, Route("Browse")]
-        public IActionResult Browse()
-        {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
