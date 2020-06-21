716fb4ccf3162e04c0970599c26aefeebb1c8139
Mihnea <mihnealopataru@gmail.com>
2020-06-21 21:09:20 +0300

    Test Commit

---
+++ silk/Silk.Data/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Silk.Web.Data
+{
+    public class Class1
+    {
+    }
+}
---
+++ silk/Silk.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Silk.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
